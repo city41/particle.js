12dc955c4b0917999a811dc231c3cc39e55c5eae
Matt Greer <matt.e.greer@gmail.com>
2012-10-27 14:22:13 -0600

    got a start on the Emitter (_initParticle)

--- public/javascripts/particlesystem/util.js
+++ public/javascripts/particlesystem/util.js
@@ -1,8 +1,33 @@
 (function() {
+	function isIntenger(num) {
+		return num === (num | 0);
+	}
+
 	this.pjs = this.pjs || {};
 
 	pjs.toRad = function(deg) {
 		return Math.PI * deg / 180;
+	};
+
+	pjs.random = function(minOrMax, maxOrUndefined, dontFloor) {
+		dontFloor = dontFloor || false;
+
+		var min = _.isNumber(maxOrUndefined) ? minOrMax: 0;
+		var max = _.isNumber(maxOrUndefined) ? maxOrUndefined: minOrMax;
+
+		var range = max - min;
+
+		var result = Math.random() * range + min;
+
+		if (isInteger(min) && isInteger(max) && !dontFloor) {
+			return Math.floor(result);
+		} else {
+			return result;
+		}
+	};
+
+	pjs.random11 = function() {
+		return pjs.random(-1, 1, true);
 	};
 
 })();
--- spec/javascripts/particlesystem/Particle.spec.js
+++ spec/javascripts/particlesystem/Particle.spec.js
@@ -1,17 +1,25 @@
 describe('Particle', function() {
 	describe('construction', function() {
-		it('should assign x and y', function() {
-			expect(new pjs.Particle(4, 0).pos.x).toEqual(4);
-			expect(new pjs.Particle(0, 6).pos.y).toEqual(6);
-		});
+		it('should default to zero', function() {
+			var p = new pjs.Particle();
 
+			expect(p.pos.x).toBe(0);
+			expect(p.pos.y).toBe(0);
+			expect(p.vel.x).toBe(0);
+			expect(p.vel.y).toBe(0);
+		});
+	});
+
+	describe('methods', function() {
 		it('should translate angle and speed to a velocity vector', function() {
 			var angle = 44;
 			var speed = 3;
-			var p = new pjs.Particle(0, 0, angle, speed);
+			var p = new pjs.Particle();
 
 			var expectedX = Math.cos(pjs.toRad(angle)) * speed;
 			var expectedY = Math.sin(pjs.toRad(angle)) * speed;
+
+			p.setVelocity(angle, speed);
 
 			expect(p.vel.x).toEqual(expectedX);
 			expect(p.vel.y).toEqual(expectedY);
