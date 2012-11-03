3059a13ded2b026e7531eb874a4c8843c7783cd4
Matt Greer <matt.e.greer@gmail.com>
2012-11-02 21:23:14 -0600

    truly pausing when mouse leaves window

--- public/javascripts/pjs.js
+++ public/javascripts/pjs.js
@@ -1,11 +1,12 @@
 (function() {
 	this.pjs = this.pjs || {};
-	
-	this.requestAnimationFrame = window.requestAnimationFrame || 
-		window.mozRequestAnimationFrame || 
-		window.webkitRequestAnimationFrame || 
+
+	this.requestAnimationFrame = window.requestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
 		window.msRequestAnimationFrame || 
-		window.oRequestAnimationFrame || function(callback) {
+		window.oRequestAnimationFrame || 
+		function(callback) {
 			window.setTimeout(function() {
 				callback(new Date().getTime());
 			}, 1 / 60 * 1000);
@@ -20,22 +21,48 @@
 		return (results && results[1]) || '';
 	}
 
+	var paused = false;
+	var lastTimestamp = 0;
+	var texture = new Image();
+	texture.src = 'particle.png';
+	var particleSystem;
+	var canvas;
+	var context;
+
+	function draw(timestamp) {
+		if (paused) {
+			return;
+		}
+
+		var delta = timestamp - (lastTimestamp || timestamp);
+		lastTimestamp = timestamp;
+
+		delta /= 1000;
+		particleSystem.update(delta);
+
+		context.fillStyle = 'black';
+		context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+		pjs.Renderer.render(context, particleSystem.particles);
+
+		requestAnimationFrame(draw);
+	}
+
 	pjs.onReady = function() {
-		var lastTimestamp = 0;
-		var texture = new Image();
-		texture.src = 'particle.png';
-		var particleSystem;
-		window.ps = particleSystem = new pjs.Emitter({
+		pjs.ps = particleSystem = new pjs.Emitter({
 			totalParticles: 300,
 			emissionRate: 50,
-			pos: { x: 200, y: 200 },
+			pos: {
+				x: 200,
+				y: 200
+			},
 			angle: 0,
 			angleVar: 360,
 			speed: 20,
 			speedVar: 10,
 			life: 7,
 			lifeVar: 4,
-			radialAccel: -20,
+			radialAccel: - 20,
 			radialAccelVar: 0,
 			tangentialAccel: 15,
 			tangentialAccelVar: 0,
@@ -50,29 +77,25 @@
 			duration: Infinity
 		});
 
-		var canvas = document.createElement('canvas');
+		canvas = document.createElement('canvas');
 		canvas.width = 350;
 		canvas.height = 400;
-		var context = canvas.getContext('2d');
+		context = canvas.getContext('2d');
 
-		function draw(timestamp) {
-			var delta = timestamp - (lastTimestamp || timestamp);
-			lastTimestamp = timestamp;
+		pjs.ui.Builder.build(pjs, particleSystem, canvas, getUrlParam('ui'), function() {
+			draw(new Date().getTime());
+		});
 
-			delta /= 1000;
-			particleSystem.update(delta);
-
-			context.fillStyle = 'black';
-			context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-
-			pjs.Renderer.render(context, particleSystem.particles);
-			
-			requestAnimationFrame(draw);
-		}
-
-		pjs.ui.Builder.build(particleSystem, canvas, getUrlParam('ui'));
-
-		draw(new Date().getTime());
 	};
+
+	pjs.pause = function() {
+		paused = true;
+	};
+
+	pjs.resume = function() {
+		paused = false;
+		lastTimestamp = 0;
+		draw(new Date().getTime());
+	}
 })();
 
