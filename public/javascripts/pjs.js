d45c19c9312b122df9697c525c8b0b897f73e0ce
Matt Greer <matt.e.greer@gmail.com>
2012-11-03 21:38:48 -0600

    added 'none' option and better initial default system

--- public/javascripts/pjs.js
+++ public/javascripts/pjs.js
@@ -58,35 +58,6 @@
 		pjs.defaultTexture.src = 'particle.png';
 
 		pjs.defaultTexture.onload = function() {
-			pjs.ps = particleSystem = new pjs.Emitter({
-				totalParticles: 300,
-				emissionRate: 50,
-				pos: {
-					x: 175,
-					y: 350
-				},
-				angle: 90,
-				angleVar: 20,
-				speed: 25,
-				speedVar: 10,
-				life: 7,
-				lifeVar: 4,
-				radialAccel: 0,
-				radialAccelVar: 0,
-				tangentialAccel: 0,
-				tangentialAccelVar: 0,
-				radius: 10,
-				radiusVar: 4,
-				startScale: 1,
-				endScale: 0,
-				texture: pjs.defaultTexture,
-				textureEnabled: true,
-				startColor: [1, 0.5, 0, 0.8],
-				endColor: [0, 0, 0, 0],
-				active: true,
-				duration: Infinity
-			});
-
 			canvas = document.createElement('canvas');
 
 			var canvasSize = getCanvasSize();
@@ -96,6 +67,8 @@
 
 			pjs.positionPredefinedSystems(canvasSize);
 			pjs.setTextureOnPredefinedSystems(pjs.defaultTexture);
+
+			pjs.ps = particleSystem = new pjs.Emitter(pjs.predefinedSystems[0].system);
 
 			context = canvas.getContext('2d');
 
